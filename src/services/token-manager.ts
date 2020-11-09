d3e17d13baac2da51fec376bed909437417e629c
Zach Ayers <ayers.dev@icloud.com>
2020-11-09 15:24:07 -0600

    Updates

--- src/services/token-manager.ts
+++ src/services/token-manager.ts
@@ -1,66 +1,66 @@
-import { Document } from "mongoose";
 import jsonwebtoken from "jsonwebtoken";
 
 // An interface that describes the properties that a User Document has
-export interface IUserDoc extends Document {
-  id?: string;
-  email: string;
-  password: string;
+export interface ISignUpPayload {
+    id?: string;
+    email: string;
+    password: string;
 }
 
 export interface IUserPayload {
-  id: string;
-  email: string;
+    id: string;
+    email: string;
 }
 
 declare global {
-  namespace Express {
-    interface Request {
-      currentUser?: IUserPayload;
+    namespace Express {
+        interface Request {
+            currentUser?: IUserPayload;
+        }
     }
-  }
 }
 
 export class TokenManager {
-  public static SignToken = (user: IUserDoc): { jwt: string } => {
-    // Generate JSON Web Token
-    const { JWT_SECRET } = process.env;
+    public static SignToken = (user: ISignUpPayload): { jwt: string } => {
+        // Generate JSON Web Token
+        const {JWT_SECRET} = process.env;
 
-    return {
-      jwt: jsonwebtoken.sign(
-        {
-          id: user.id,
-          email: user.email,
-        },
-        JWT_SECRET!
-      ),
-    };
-  };
-
-  public static SpoofToken = (): [string] => {
-    // Build a JWT Payload {id, email, password}
-    const payload = {
-      id: "abcd1234",
-      email: "test@test.com",
-      password: "abcd1234",
+        return {
+            jwt: jsonwebtoken.sign(
+                {
+                    id: user.id,
+                    email: user.email,
+                },
+                JWT_SECRET!
+            ),
+        };
     };
 
-    // Create the JWT
-    const session = TokenManager.SignToken(payload as IUserDoc);
+    public static SpoofToken = (): [string] => {
+        // Build a JWT Payload {id, email, password}
+        const payload = {
+            id: "abcd1234",
+            email: "test@test.com",
+            password: "abcd1234",
+        };
 
-    // Turn the session into JSON
-    const sessionJSON = JSON.stringify(session);
+        // Create the JWT
+        const session = TokenManager.SignToken(payload);
 
-    // Take the JSON and encode it as base64
-    const base64Session = Buffer.from(sessionJSON).toString("base64");
+        // Turn the session into JSON
+        const sessionJSON = JSON.stringify(session);
 
-    // Return the cookie with the encoded data as a string
-    // Supertest prefers the strings to be in an array
-    return [`express:sess=${base64Session}`];
-  };
+        // Take the JSON and encode it as base64
+        const base64Session = Buffer.from(sessionJSON)
+                                    .toString("base64");
 
-  public static VerifyToken = (token: string): IUserPayload => {
-    const { JWT_SECRET } = process.env;
-    return jsonwebtoken.verify(token, JWT_SECRET!) as IUserPayload;
-  };
+        // Return the cookie with the encoded data as a string
+        // Supertest prefers the strings to be in an array
+        return [`express:sess=${base64Session}`];
+    };
+
+    public static VerifyToken = (token: string): IUserPayload => {
+        const {JWT_SECRET} = process.env;
+        return jsonwebtoken.verify(token, JWT_SECRET!) as IUserPayload;
+    };
 }
