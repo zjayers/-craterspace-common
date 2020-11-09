d0f93da97d64f8a4bcaa38ff78ddd1b71d024f47
Zach Ayers <ayers.dev@icloud.com>
2020-11-09 15:04:14 -0600

    Updates

--- src/services/token-manager.ts
+++ src/services/token-manager.ts
@@ -37,6 +37,28 @@ export class TokenManager {
     };
   };
 
+  public static SpoofToken = (): [string] => {
+    // Build a JWT Payload {id, email, password}
+    const payload = {
+      id: "abcd1234",
+      email: "test@test.com",
+      password: "abcd1234",
+    };
+
+    // Create the JWT
+    const session = TokenManager.SignToken(payload as IUserDoc);
+
+    // Turn the session into JSON
+    const sessionJSON = JSON.stringify(session);
+
+    // Take the JSON and encode it as base64
+    const base64Session = Buffer.from(sessionJSON).toString("base64");
+
+    // Return the cookie with the encoded data as a string
+    // Supertest prefers the strings to be in an array
+    return [`express:sess=${base64Session}`];
+  };
+
   public static VerifyToken = (token: string): IUserPayload => {
     const { JWT_SECRET } = process.env;
     return jsonwebtoken.verify(token, JWT_SECRET!) as IUserPayload;
